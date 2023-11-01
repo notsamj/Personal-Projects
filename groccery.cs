166935b1154b1fd62b8878712fa84937b2cd71e2
Samuel Jones <samuel.jones1@ontariotechu.net>
2023-11-01 15:40:19 -0400

    Now able to display an empty list

--- groccery.cs
+++ groccery.cs
@@ -6,26 +6,30 @@ public class GrocceryApplication {
     static public void Main(){
         Console.WriteLine("Groccery Application Started!");
         grocceryList = new GrocceryList();
-        grocceryList.print();
+        grocceryList.defaultPrint();
     }
 }
 
 public class GrocceryList {
-    private SinglyLinkedList<int> grocceryItems;
+    private SinglyLinkedList<GrocceryItem> grocceryItems;
     
     public GrocceryList(){
-        this.grocceryItems = new SinglyLinkedList<int>();   
+        this.grocceryItems = new SinglyLinkedList<GrocceryItem>();   
     }
 
-    public void print(){
-        Console.WriteLine("Test");
+    public void defaultPrint(){
+        int grocceryListLength = this.grocceryItems.getLength();
+        Console.WriteLine("GrocceryList (" + grocceryListLength + "):");
+        for (int i = 0; i < this.grocceryItems.getLength(); i++){
+            Console.WriteLine(grocceryItems.getItem(i).getName());
+        }
     }
 }
 
 public class SinglyLinkedList<T> {
     private SLLNode<T> head;
 
-    public class SLLNode<S>{
+    public class SLLNode<T>{
         private T data;
         private SLLNode<T> next;
 
@@ -33,13 +37,49 @@ public class SinglyLinkedList<T> {
             this.data = data;
             this.next = null;
         }
+
+        public SLLNode<T> getNext(){
+            return this.next;
+        }
+
+        public T getData(){
+            return this.data;
+        }
     }
 
     public SinglyLinkedList(){
         this.head = null;
     }
+
+    public int getLength(){
+        int length = 0;
+        SLLNode<T> currentItem = this.head;
+        while (currentItem != null){
+            currentItem = currentItem.getNext();
+            length++;
+        }
+        return length;
+    }
+
+    // Returns null if itemIndex > length(linked_list)
+    public T getItem(int itemIndex){
+        int currentIndex = 0;
+        SLLNode<T> currentItem = this.head;
+        while(currentIndex < itemIndex){
+            currentItem = currentItem.getNext();
+            currentIndex++;
+        }
+        return currentItem.getData();
+    }
 }
 
 public class GrocceryItem{
+    private String name;
     
+    public GrocceryItem(String name){
+        this.name = name;
+    }
+
+    public String getName(){ return this.name; }
+    public void setName(String name){ this.name = name; }
 }
