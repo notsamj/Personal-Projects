8bd7ab2ef57d27c6afd761a5241d9af9f2029816
notsamj <samyoul.jones@gmail.com>
2023-11-09 14:19:59 -0500

    Significant progress on Grocery List App

--- JavaScript App 1/scripts/grocery.js
+++ JavaScript App 1/scripts/grocery.js
@@ -1,3 +1,51 @@
 console.log("Grocery Script Running!");
 
-// Temp
+/*
+    Notes:
+        - Assuming that elements exist otherwise get an error. It's fine user can delete and give themself and error.
+*/
+
+// Global Variables
+var groceryList = new GroceryList();
+
+// Functions
+function newItem(){
+    let textArea = document.getElementById("newListItemTextArea");
+    let text = textArea.value;
+    if (text === ""){
+        return;
+    }
+
+    // Reset the text
+    textArea.value = "";
+    groceryList.addItem(text);
+    resetItemDisplay();
+}
+
+function resetItemDisplay(){
+    resetItemDetails();
+    resetItemList();
+}
+
+function resetItemDetails(){
+    // TODO
+}
+
+function resetItemList(){
+    // TODO
+}
+
+function getData(){
+    console.log(groceryList.toDataString());
+}
+
+function enterData(dataJSON){
+    groceryList.fromDataJSON(dataJSON);
+}
+
+
+
+// Listeners
+addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("newListItem").addEventListener("keypress", function (event){ if (event.key === "Enter"){ newItem(); event.preventDefault(); }});
+}); 
---
+++ JavaScript App 1/scripts/grocery_list_class.js
@@ -0,0 +1,18 @@
+console.log("GroceryList Class Loaded!");
+class GroceryList{
+    constructor(){
+        this._listData = {"items":[]};
+    }
+
+    addItem(name){
+        this._listData["items"].push({"name": name});
+    }
+
+    toDataString(){
+        return JSON.stringify(this._listData);
+    }
+
+    fromDataJSON(dataJSON){
+        this._listData = dataJSON;
+    }
+}
