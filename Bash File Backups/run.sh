d60a7a393db8df000d2da34d1f94c9fcb54c66aa
samuel-jones-uoit <samyoul.jones@gmail.com>
2024-08-13 16:28:36 -0400

    Calling it a day

--- Bash File Backups/run.sh
+++ Bash File Backups/run.sh
@@ -1,10 +1,82 @@
 # Functions
+ensurePathExists(){
+	# $1 - absolute path to back up folder
+	# $2 - additional path
+	local additionalPathStr="$2"
+	local splitAdditionalPathArray=(${additionalPathStr//\//})
+	echo "$1"
+	cd "$1"
+	local tempPath="$1"
+	for pathPart in "${splitAdditionalPathArray[@]}"; do
+		tempPath="$tempPath$pathPart/"
+		echo "aa $tempPath"
+		if [[ ! (-d $tempPath) ]]; then
+			mkdir "$pathPart"
+		fi
+		cd "$pathPart"
+	done
+}
+
+moveNewFiles(){
+	# $1 - in folder absolute path
+	# $2 - backup folder absolute path
+	# $3 - additional path
+	local fullPath="$1$3"
+	local fullPathInBackup="$1$3"
+	cd "$fullPath"
+	files=(*)
+	for fileOrDir in "${files[@]}"; do
+		if [ "$fileOrDir" = "*" ]; then
+			return 0 # empty directory
+		fi
+		local fullPathToFileOrDir="$fullPath$fileOrDir"
+		if [[ -d $fullPathToFileOrDir ]]; then
+		    moveNewFiles "$1" "$2" "$3$fileOrDir/"
+		elif [[ -f $fullPathToFileOrDir ]]; then
+			# Make sure the path exists in backup
+			#echo ensuring "$2"
+			ensurePathExists "$2" "$3"
+			# Run shasum command
+			checksum=$(shasum -a 256 "$fullPathToFileOrDir")
+			# Isolate the checksum (first 64 characters)
+			checksum=${checksum:0:64}
+			# Add check sum if its not present
+			findCheckSumCountOrAdd $checksum
+			# If count is 1 then copy to the backup
+			if [ $checkSumCount = 1 ]; then
+				cp "$fullPathToFileOrDir" "$fullPathInBackup"
+			fi
+		else
+		    echo Found an invalid element "$fullPathToFileOrDir"
+		    exit 1
+		fi
+	done
+}
+
+createNewBackupFolder(){
+	local i=1
+	local newBackUpFolderName="backup_$i"
+	local newBackUpFolderPath="$outFolderAPath$newBackUpFolderName"
+	# While this backup name is taken (folder exists in output folder)
+	while [[ -d $newBackUpFolderPath ]]
+	do
+		# Increase i
+		i=$((i + 1))
+		local newBackUpFolderName="backup_$i"
+		local newBackUpFolderPath="$outFolderAPath$newBackUpFolderName"
+	done
+
+	# Create new backup folder
+	mkdir newBackUpFolderPath
+	backupFolderNumber=$i
+}
+
 findCheckSumCountOrAdd(){
 	local i=0
 	for hash in "${hashes[@]}"; do
 		if [ "$1" = "$hash" ]; then
-			hash_counts[i]=$((hash_counts[i] + 1))
-		  	checkSumCount=${hash_counts[$i]}
+			hashCounts[i]=$((hashCounts[i] + 1))
+		  	checkSumCount=${hashCounts[$i]} # output global variable
 			return 0 # success
 		fi
 
@@ -13,51 +85,63 @@ findCheckSumCountOrAdd(){
 	done
 	# Not found, add it
 	hashes+=("$1")
-	hash_counts+=(1)
+	hashCounts+=(1)
+	checkSumCount=-1 # if not found
 	return 0 # success
 }
 
-exploreDirectory(){
-	# Navigate by to the directory
-	cd "$1"
+collectChecksumsFromDirectory(){
+	# $1 - out folder
+	# $2 - extra path
 
+	# cd to full path
+	#echo $1
+	#echo $2
+	local fullPath="$1$2"
+	cd "$fullPath"
 	files=(*)
 	for fileOrDir in "${files[@]}"; do
-		if [[ -d $fileOrDir ]]; then
-		    exploreDirectory $fileOrDir
-		elif [[ -f $fileOrDir ]]; then
+		if [ "$fileOrDir" = "*" ]; then
+			return 0 # empty directory
+		fi
+		local fullPathToFileOrDir="$fullPath$fileOrDir"
+		#echo fptfod "$fullPathToFileOrDir"
+		#echo argtwonexttime "$2$fileOrDir/"
+		if [[ -d $fullPathToFileOrDir ]]; then
+		    collectChecksumsFromDirectory "$1" "$2$fileOrDir/"
+		elif [[ -f $fullPathToFileOrDir ]]; then
 			# Run shasum command
-			checksum=$(shasum -a 256 "$fileOrDir")
+			checksum=$(shasum -a 256 "$fullPathToFileOrDir")
 			# Isolate the checksum (first 64 characters)
 			checksum=${checksum:0:64}
 			# Add check sum if its not present
 			findCheckSumCountOrAdd $checksum
 		else
-		    echo Found an invalid element "$fileOrDir" in directory "$1"
+		    echo Found an invalid element "$fullPathToFileOrDir"
 		    exit 1
 		fi
 	done
-	# Navigate back from the directory
-	cd ..
 }
 
 # Clear the screen
 clear
 
-# Remove the out folder
-rm -rf out
-mkdir out
+inFolderAPath="/Users/samueljones/Documents/GitHub/Personal-Projects/Bash File Backups/in/"
+outFolderAPath="/Users/samueljones/Documents/GitHub/Personal-Projects/Bash File Backups/out/"
+backupFolderNumber=-1 # placeholderr value
 
-# Get counts for all the hashes
+# Stores hashes and associated counts
 hashes=()
-hash_counts=()
+hashCounts=() # Count's aren't really needed but I'd like to be able to review this code in the future so its nice to have
 
-# Explore the test folder
-exploreDirectory test_folder
+# Explore the out folder to see what files are already backed up
+collectChecksumsFromDirectory "$outFolderAPath" ""
 
+# Create new backup folder
+createNewBackupFolder
 
-# Print all hashes
-echo Printing all found hashes
-for hash in "${hashes[@]}"; do
-	echo $hash
-done
+# backupFolderNumber variable now has a meaningful value
+backupFolderPath="$outFolderAPath""backup_""$backupFolderNumber"
+
+# Move new files to backup directory
+moveNewFiles "$inFolderAPath" "$backupFolderPath" ""
